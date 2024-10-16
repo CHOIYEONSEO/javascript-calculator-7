9306b958a7241f2e26d4224c340096f7c4813cbe
YEONSEO <yeonseochoi0717@gmail.com>
2024-10-16 20:08:43 +0900

    커스텀 구분자 처리

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import MakeSeparator from "./MakeSeparator.js";
 
 class App {
   async run() {
-    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-    //const separators = [',', ':'];
+    let input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
     const separator = MakeSeparator();
+    //Console.print(separator);
+
+    while (input.match(/^\/\//g)){
+      const match = input.match(/\/\/(.*?)\\n/);
+      //Console.print(match);
+      const customSep = match[1];
+      separator.push(customSep);
+      input = input.substr(match[0].length);
+    }
+    //Console.print(input);
     //Console.print(separator);
   }
 }
