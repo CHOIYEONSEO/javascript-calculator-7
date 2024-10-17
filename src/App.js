ebba1bfabcbfc0a2d35c4f2411376024829ac331
YEONSEO <yeonseochoi0717@gmail.com>
2024-10-17 22:44:07 +0900

    feat: 커스텀 구분자가 아닌 문자가 포함되었을 때 에러 처리

--- src/App.js
+++ src/App.js
@@ -4,30 +4,36 @@ import MakeSeparator from "./MakeSeparator.js";
 
 class App {
   async run() {
-    let input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-    const separator = MakeSeparator();
-
-    while(input.match(/^\/\//g)) {
-      const match = input.match(/\/\/(.*?)\\n/);
-      const customSep = match[1];
-      separator.push(customSep);
-      input = input.substr(match[0].length);
-    }
-
-    const separatorRegex = new RegExp(`[${separator.join('')}]`);
-
-    const numbers = input.split(separatorRegex).map(num => {
-      if(isNaN(num)){
-        //Error, 애플리케이션 종료
+    try{
+      let input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+      const separator = MakeSeparator();
+  
+      while(input.match(/^\/\//g)) {
+        const match = input.match(/\/\/(.*?)\\n/);
+        const customSep = match[1];
+        separator.push(customSep);
+        input = input.substr(match[0].length);
       }
-      return parseFloat(num);
-    })
+  
+      const separatorRegex = new RegExp(`[${separator.join('')}]`);
+  
+      const numbers = input.split(separatorRegex).map(num => {
+        if(isNaN(num)){
+          const errMessage = `[ERROR] 적절하지 않은 입력 값`;
+          throw new Error(errMessage);
+        }
 
-    const result = +(numbers.reduce((acc, cur) => acc + cur, 0)).toFixed(12);
-
-    const message = `결과 : ${result}`;
-
-    Console.print(message);
+        return parseFloat(num);
+      })
+  
+      const result = +(numbers.reduce((acc, cur) => acc + cur, 0)).toFixed(12);
+  
+      const message = `결과 : ${result}`;
+  
+      Console.print(message);
+    } catch (error) {
+      Console.print(error.message);
+    }
   }
 }
 
