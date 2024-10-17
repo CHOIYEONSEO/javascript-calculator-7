99e17d8c3163c366c7fa1ca5f92701ab0622ed15
YEONSEO <yeonseochoi0717@gmail.com>
2024-10-17 22:00:21 +0900

    refactor: 주석 제거

--- src/App.js
+++ src/App.js
@@ -4,33 +4,18 @@ import MakeSeparator from "./MakeSeparator.js";
 
 class App {
   async run() {
-    //let result = 0;
     let input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
     const separator = MakeSeparator();
-    //Console.print(separator);
 
     while(input.match(/^\/\//g)) {
       const match = input.match(/\/\/(.*?)\\n/);
-      //Console.print(match);
       const customSep = match[1];
       separator.push(customSep);
       input = input.substr(match[0].length);
     }
-    //Console.print(input);
-    //Console.print(separator);
 
     const separatorRegex = new RegExp(`[${separator.join('')}]`);
-/* 첫 시도 - 실수 합 결과 ex)2.4,3.3 5.69999999로 표시됨
-    const numbers = input.split(separatorRegex);
-    //Console.print(numbers);
 
-    for(let num of numbers) {
-      if(isNaN(num)){
-        //Error, 애플리케이션 종료
-      }
-      result += parseFloat(num);
-    }
-*/
     const numbers = input.split(separatorRegex).map(num => {
       if(isNaN(num)){
         //Error, 애플리케이션 종료
