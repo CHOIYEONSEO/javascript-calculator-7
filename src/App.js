6d70bfb610bde67bf8837df39b9e9caf6ed6033a
YEONSEO <yeonseochoi0717@gmail.com>
2024-10-18 21:58:30 +0900

    refactor: 변수명 통일

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App {
         throw new Error(errMessage);
       }
   
-      const numbers = input.split(separatorRegex).map(num => {
+      const numbers = splitArr.map(num => {
         if(isNaN(num)){
           const errMessage = `[ERROR] 적절하지 않은 입력 값(Invalid Input: ${num})`;
           throw new Error(errMessage);
