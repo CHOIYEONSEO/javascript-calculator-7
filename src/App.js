62bf47e503ee5577e070aa3107164af59eb46ed3
YEONSEO <yeonseochoi0717@gmail.com>
2024-10-18 22:10:43 +0900

    에러 메시지 수정

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App {
   
       const numbers = splitArr.map(num => {
         if(isNaN(num)){
-          const errMessage = `[ERROR] 적절하지 않은 입력 값(Invalid Input: ${num})`;
+          const errMessage = `[ERROR] 적절하지 않은 입력 값(Not Number: ${num})`;
           throw new Error(errMessage);
         }
 
